d780ed02423cf19d72a9704a58557f89d4c2b8e8
GRy82 <gwryan@buffalo.edu>
2020-11-10 21:33:21 -0500

    Same updates as before but now made to herd/dinosaurs-- attacking menu created, attack system update

--- RobotsAndDinosaurs/Dinosaur.cs
+++ RobotsAndDinosaurs/Dinosaur.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProblemSolving1;
 
 namespace RobotsAndDinosaurs
 {
@@ -41,16 +42,50 @@ namespace RobotsAndDinosaurs
 
         public double Attack()
         {
+            int attackOption = SelectAttackMenu();
+            double attackValue = SwitchAttack(attackOption);
             Random randAttack = new Random();
-            double randomDeviation = attackPower * .15;
+            double randomDeviation = attackValue * .15;
             randomDeviation = Math.Round(randomDeviation);
-            int lowerLimit = Convert.ToInt32(attackPower - randomDeviation);
-            int upperLimit = Convert.ToInt32(attackPower + randomDeviation);
+            int lowerLimit = Convert.ToInt32(attackValue - randomDeviation);
+            int upperLimit = Convert.ToInt32(attackValue + randomDeviation);
             double resultantAttack = randAttack.Next(lowerLimit, upperLimit);
             return resultantAttack;
         }
 
+        public void DisplayAttackTypes()
+        {
+            Console.WriteLine("______________________________________________");
+            Console.WriteLine("| Attack Type  | %-weapon dmg | Cost(energy) |");
+            Console.WriteLine("|     Claw     |     100      |      10      |");
+            Console.WriteLine("|     Bite     |     120      |      15      |");
+            Console.WriteLine("|    Pounce    |     140      |      20      |");
+            Console.WriteLine("|______________|______________|______________|");
 
+        }
 
+        public int SelectAttackMenu()
+        {
+            List<string> attackOptions = new List<string> { "Claw", "Bite", "Pounce" };
+            ConsoleOptionsInterface attackMenu = new ConsoleOptionsInterface(attackOptions, false);
+            Console.WriteLine("\nChoose how to attack.");
+            int option = attackMenu.Launch();
+            return option;
+        }
+
+        public double SwitchAttack(int attackOption)
+        {
+            switch (attackOption)
+            {
+                case 1:
+                    return attackPower;
+                case 2:
+                    return attackPower * 1.2;
+                case 3:
+                    return attackPower * 1.4;
+                default:
+                    return 0;
+            }
+        }
     }
 }
